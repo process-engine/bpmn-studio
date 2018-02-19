a64ba88aeca38bad255897abcb09e985ef0e3287
Alex <alexander.kasten@5minds.de>
2018-02-19 11:09:22 +0100

    :bug: Add selection changed event for rendering also elements if selection has changed

--- src/modules/property-panel/indextabs/general/general.ts
+++ src/modules/property-panel/indextabs/general/general.ts
@@ -49,15 +49,19 @@ export class General implements IIndextab {
   public checkElement(element: IModdleElement): boolean {
     return true;
   }
+
   public activate(model: IPageModel): void {
     this.eventBus = model.modeler.get('eventBus');
 
-    this.eventBus.on(['element.click', 'shape.changed'], (event: IEvent) => {
+    this.eventBus.on(['element.click', 'shape.changed', 'selection.changed'], (event: IEvent) => {
       if (event.type === 'element.click') {
         this.elementInPanel = event.element.businessObject;
       }
       if (event.type === 'shape.changed' && event.element.type !== 'label') {
         this.elementInPanel = event.element.businessObject;
+      }
+      if (event.type === 'selection.changed' && event.newSelection.length !== 0) {
+        this.elementInPanel = event.newSelection[0].businessObject;
       }
       if (this.elementInPanel) {
         this.sections.forEach((section: ISection) => {
