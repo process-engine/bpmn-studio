c81a2e667ce041d0630bfc6855d5ced21d9943d1
Steffen Knaup <Steffen.Knaup@5minds.de>
2018-05-24 12:01:07 +0200

    :sparkles: Add Router to Process Solution Explorer

--- src/modules/process-solution-panel/process-solution-panel.ts
+++ src/modules/process-solution-panel/process-solution-panel.ts
@@ -6,20 +6,23 @@ import {
   IProcessDefEntity,
 } from '@process-engine/bpmn-studio_client';
 import {EventAggregator, Subscription} from 'aurelia-event-aggregator';
+import { Router } from 'aurelia-router';
 import {AuthenticationStateEvent} from '../../contracts/index';
 import environment from '../../environment';
 
-@inject('BpmnStudioClient', EventAggregator)
+@inject('BpmnStudioClient', EventAggregator, Router)
 export class ProcessSolutionPanel {
   private _bpmnStudioClient: BpmnStudioClient;
   private _subscriptions: Array<Subscription>;
   private _eventAggregator: EventAggregator;
+  private _router: Router;
 
   public processes: IPagination<IProcessDefEntity>;
 
-  constructor(bpmnStudioClient: BpmnStudioClient, eventAggregator: EventAggregator) {
+  constructor(bpmnStudioClient: BpmnStudioClient, eventAggregator: EventAggregator, router: Router) {
     this._bpmnStudioClient = bpmnStudioClient;
     this._eventAggregator = eventAggregator;
+    this._router = router;
     this._refreshProcesslist();
   }
 
