e54534c09323196d18852913f3d70250ab742e26
Vyperus <bastian.schnittger@5minds.de>
2018-12-27 10:36:35 +0100

    Register Config Service as Singleton

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,8 +1,10 @@
 import {FrameworkConfiguration} from 'aurelia-framework';
 
+import {ConfigService} from './ConfigService';
 import {SolutionService} from './SolutionService';
 
 export async function configure(config: FrameworkConfiguration): Promise<void> {
 
   config.container.registerSingleton('SolutionService', SolutionService);
+  config.container.registerSingleton('ConfigService', ConfigService);
 }
