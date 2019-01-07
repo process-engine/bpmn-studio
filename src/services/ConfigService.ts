43ffb933f7e881f16eb60da7e62061542794495f
Steffen Knaup <steffen.knaup@5minds.de>
2019-01-07 09:57:52 +0100

    :bug: Fix Typo
    Co-Authored-By: Vyperus <bastian.schnittger@5minds.de>

--- src/services/ConfigService.ts
+++ src/services/ConfigService.ts
@@ -3,7 +3,7 @@ import {IConfigService} from '../contracts';
 /**
  * The goal of this service is to keep most configurations out of the normal
  * business code. Especially persisting them in the localstorage and getting
- * them back at launch of the application are the main tasks of this service.
+ * them back at the launch of the application are the main tasks of this service.
  *
  * This will keep all configurations in one place and will prevent setting and
  * getting the configurations from spreading all over the applications code.
