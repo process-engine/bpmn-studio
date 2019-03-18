64593f4f17916bfe219eab56f61310a6a2c40b46
Alex😎 <alexander.kasten@5minds.de>
2019-03-18 14:12:44 +0100

    :sparkles: Use getOpenedTemporarySingleDiagramByName on Inspect
    Co-Authored-By: Christian W. <elraptorus@users.noreply.github.com>

--- src/modules/inspect/inspect.ts
+++ src/modules/inspect/inspect.ts
@@ -167,12 +167,8 @@ export class Inspect {
         const noActiveDiagram: boolean = this.activeDiagram === undefined;
         if (noActiveDiagram) {
           const service: SingleDiagramsSolutionExplorerService = this.activeSolutionEntry.service as SingleDiagramsSolutionExplorerService;
-          const allOpenedSingleDiagrams: Array<IDiagram> = service.getOpenedDiagrams();
 
-          this.activeDiagram = allOpenedSingleDiagrams.find((diagram: IDiagram) => {
-            return diagram.name === diagramName
-                && diagram.uri.includes('temp-diagrams');
-          });
+          this.activeDiagram = service.getOpenedTemporarySingleDiagramByName(diagramName);
         }
       } else {
 
