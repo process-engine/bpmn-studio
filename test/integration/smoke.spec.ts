4323d3df473c8793e1f276dc28d3d0cfacf9e288
Alex😎 <alexander.kasten@5minds.de>
2019-09-02 09:40:33 +0200

    :lipstick: Adjust file after rebase

--- test/integration/smoke.spec.ts
+++ test/integration/smoke.spec.ts
@@ -24,7 +24,7 @@ function getApplicationArgs(givenPath: string | null): any {
 
   const electronExecutable = isWindows ? 'electron.cmd' : 'electron';
   const electronPath = path.join(__dirname, '..', '..', 'node_modules', '.bin', electronExecutable);
-  const electronBundlePath = path.join(__dirname, '..', '..', 'electron_app', 'electron.js');
+  const electronBundlePath = path.join(__dirname, '..', '..', 'dist', 'electron_app', 'electron_app', 'electron.js');
 
   return {...commonArgs, path: electronPath, args: [electronBundlePath]};
 }
@@ -153,89 +153,11 @@ describe('Application launch', function foo() {
     await testClient.openDesignViewFromStatusbar();
   });
 
-  it('should open a solution', async () => {
-    await testClient.ensureVisible('h3=Welcome to BPMN Studio!');
-
-    await testClient.openDirectoryAsSolution('fixtures');
-    await testClient.ensureVisible('.solution-entry__solution-name=fixtures');
-  });
-
   it('should create and open a new diagam by clicking on new diagram link', async () => {
     await createAndOpenDiagram();
 
     await testClient.assertNavbarTitleIs('Untitled-1');
     await testClient.assertWindowTitleIs('Design | BPMN Studio');
-  });
-
-  it('should render a diagram correctly', async () => {
-    await createAndOpenDiagram();
-
-    await testClient.ensureVisible('[data-element-id="Collaboration_1cidyxu"]');
-  });
-
-  it('should select StartEvent after opening a diagram', async () => {
-    await createAndOpenDiagram();
-    await testClient.showPropertyPanel();
-
-    await testClient.assertSelectedBPMNElementHasName('Start Event');
-  });
-
-  it('should select element on click', async () => {
-    await createAndOpenDiagram();
-    await testClient.showPropertyPanel();
-
-    const elementName = 'End Event';
-
-    await testClient.rejectSelectedBpmnElementHasName(elementName);
-    await testClient.clickOnBpmnElementWithName(elementName);
-    await testClient.assertSelectedBpmnElementHasName(elementName);
-  });
-
-  it('should create and open a second diagram', async () => {
-    await createAndOpenDiagram();
-
-    await testClient.assertNavbarTitleIs('Untitled-1');
-    await testClient.assertWindowTitleIs('Design | BPMN Studio');
-
-    creatingFirstDiagram = false;
-    await createAndOpenDiagram();
-
-    await testClient.assertNavbarTitleIs('Untitled-2');
-  });
-
-  it('should open detail view', async () => {
-    await createAndOpenDiagram();
-    await testClient.openStartPage();
-
-    await testClient.openDesignViewForDiagram(
-      'Untitled-1',
-      'about:open-diagrams/Untitled-1.bpmn',
-      'about:open-diagrams',
-    );
-  });
-
-  it('should open XML view', async () => {
-    await createAndOpenDiagram();
-
-    await testClient.openXmlViewForDiagram('Untitled-1', 'about:open-diagrams/Untitled-1.bpmn', 'about:open-diagrams');
-  });
-
-  it('should open diff view', async () => {
-    await createAndOpenDiagram();
-
-    await testClient.openDiffViewForDiagram('Untitled-1', 'about:open-diagrams/Untitled-1.bpmn', 'about:open-diagrams');
-  });
-
-  it('should open the xml view from the status bar', async () => {
-    await createAndOpenDiagram();
-    await testClient.openXmlViewFromStatusbar();
-  });
-
-  it('should open design view from the status bar', async () => {
-    await createAndOpenDiagram();
-
-    await testClient.openXmlViewFromStatusbar();
-    await testClient.openDesignViewFromStatusbar();
   });
 
   it('should open a solution', async () => {
