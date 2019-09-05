0c54607cea50a38b6e5d22fba92489132efa2604
Alex😎 <alexander.kasten@5minds.de>
2019-09-05 15:54:46 +0200

    :sparkles: Add test-class for XmlView

---
+++ test/integration/test-classes/xml-view.ts
@@ -0,0 +1,29 @@
+/* eslint-disable @typescript-eslint/no-useless-constructor */
+/* eslint-disable no-useless-constructor */
+import {Application} from 'spectron';
+import assert from 'assert';
+
+import {GlobalMethods} from './global-methods';
+
+export class XmlView extends GlobalMethods {
+  constructor(app: Application) {
+    super(app);
+  }
+
+  public async open(diagramName: string, diagramUri: string, solutionUri?: string): Promise<void> {
+    await this.openDesignView('xml', diagramName, diagramUri, solutionUri);
+    await this.ensureVisible('[data-test-bpmn-xml-view]');
+  }
+
+  public async hasContent(): Promise<void> {
+    const xmlViewContent = await this.getTextFromElement('[data-test-xml-view-content]');
+    assert.notEqual(xmlViewContent, null);
+  }
+
+  public async assertContainsText(text: string): Promise<void> {
+    const xmlViewContent = await this.getTextFromElement('[data-test-xml-view-content]');
+    const xmlViewContentContainsText: boolean = xmlViewContent.includes(text);
+
+    assert.equal(xmlViewContentContainsText, true);
+  }
+}
