58201505c17cc1f7e376df5656de0126896259ed
Alex😎 <alexander.kasten@5minds.de>
2019-09-05 15:53:57 +0200

    :sparkles: Add test-class for PropertyPanel

---
+++ test/integration/test-classes/property-panel.ts
@@ -0,0 +1,44 @@
+/* eslint-disable @typescript-eslint/no-useless-constructor */
+/* eslint-disable no-useless-constructor */
+import {Application} from 'spectron';
+import assert from 'assert';
+import {GlobalMethods} from './global-methods';
+
+export class PropertyPanel extends GlobalMethods {
+  constructor(app: Application) {
+    super(app);
+  }
+
+  public async show(): Promise<void> {
+    const propertyPanelIsVisible = await this.webdriverClient.isVisible('[data-test-property-panel]');
+    if (propertyPanelIsVisible) {
+      return;
+    }
+
+    await this.ensureVisible('[data-test-toggle-propertypanel]');
+    await this.clickOn('[data-test-toggle-propertypanel]');
+  }
+
+  public async hide(): Promise<void> {
+    const propertyPanelIsVisible = await this.webdriverClient.isVisible('[data-test-property-panel]');
+    const propertyPanelIsHidden = !propertyPanelIsVisible;
+    if (propertyPanelIsHidden) {
+      return;
+    }
+
+    await this.ensureVisible('[data-test-toggle-propertypanel]');
+    await this.clickOn('[data-test-toggle-propertypanel]');
+  }
+
+  public async assertSelectedBpmnElementHasName(name): Promise<void> {
+    const selectedElementText = await this.getValueFromElement('[data-test-property-panel-element-name]');
+
+    assert.equal(selectedElementText, name);
+  }
+
+  public async rejectSelectedBpmnElementHasName(name): Promise<void> {
+    const selectedElementText = await this.getValueFromElement('[data-test-property-panel-element-name]');
+
+    assert.notEqual(selectedElementText, name);
+  }
+}
