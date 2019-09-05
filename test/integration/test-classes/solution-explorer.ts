20f9136c4c28299ec7b3ade8c008d1fcf0f664d2
Alex😎 <alexander.kasten@5minds.de>
2019-09-05 15:54:07 +0200

    :sparkles: Add test-class for SolutionExplorer

---
+++ test/integration/test-classes/solution-explorer.ts
@@ -0,0 +1,74 @@
+/* eslint-disable @typescript-eslint/no-useless-constructor */
+/* eslint-disable no-useless-constructor */
+import path from 'path';
+
+import {Application} from 'spectron';
+import {GlobalMethods} from './global-methods';
+
+export class SolutionExplorer extends GlobalMethods {
+  constructor(app: Application) {
+    super(app);
+  }
+
+  public async show(): Promise<void> {
+    const solutionExplorerIsVisible = await this.webdriverClient.isVisible('[data-test-solution-explorer-panel]');
+    if (solutionExplorerIsVisible) {
+      return;
+    }
+
+    await this.ensureVisible('[data-test-toggle-solution-explorer]');
+    await this.clickOn('[data-test-toggle-solution-explorer]');
+    await this.ensureVisible('[data-test-solution-explorer-panel]');
+  }
+
+  public async hide(): Promise<void> {
+    const solutionExplorerIsVisible = await this.webdriverClient.isVisible('[data-test-solution-explorer-panel]');
+    const solutionExplorerIsHidden = !solutionExplorerIsVisible;
+    if (solutionExplorerIsHidden) {
+      return;
+    }
+
+    await this.ensureVisible('[data-test-toggle-solution-explorer]');
+    await this.clickOn('[data-test-toggle-solution-explorer]');
+    await this.ensureNotVisible('[data-test-solution-explorer-panel]');
+  }
+
+  public async assertInternalProcessEngineIsOpenedAsSolution(): Promise<void> {
+    await this.ensureVisible('[data-test-solution-is-internal=true]');
+  }
+
+  public async openDirectoryAsSolution(dir: string, diagramName?: string): Promise<void> {
+    const pathToSolution: string = path.join(__dirname, '..', '..', '..', dir);
+
+    await this.webdriverClient.executeAsync(
+      async (solutionPath, solutionIdentity, done) => {
+        // eslint-disable-next-line no-underscore-dangle
+        await (window as any).__dangerouslyInvoke.openSolution(solutionPath, false, solutionIdentity);
+
+        done();
+      },
+      pathToSolution,
+      this.getDefaultIdentity(),
+    );
+
+    await this.ensureVisible(`[data-test-solution-entry-name=${dir}]`);
+
+    if (diagramName) {
+      const searchString = this.getUriForSelector(pathToSolution, diagramName);
+      await this.ensureVisible(`[data-test-open-diagram-with-uri*="${searchString}"]`);
+      await this.clickOn(`[data-test-open-diagram-with-uri*="${searchString}"]`);
+    }
+  }
+
+  private getUriForSelector(pathToSolution: string, diagramName: string): string {
+    const isWindows = process.platform === 'win32';
+    if (isWindows) {
+      const searchString: string = `${pathToSolution}\\${diagramName}`;
+      const replacedSearchString = searchString.replace(/[\\]/gm, '\\\\');
+
+      return replacedSearchString;
+    }
+
+    return `${pathToSolution}/${diagramName}`;
+  }
+}
