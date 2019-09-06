3e909531d8ecc14e19373d31dad2cd64a6c13638
Alex😎 <alexander.kasten@5minds.de>
2019-09-06 14:14:00 +0200

    :sparkles: Add solution-explorer.spec

---
+++ test/integration/solution-explorer.spec.ts
@@ -0,0 +1,63 @@
+import {TestClient} from './TestClient';
+import {applicationArgs} from './get-application-args';
+
+let testClient: TestClient;
+
+describe('SolutionExplorer', function foo() {
+  this.slow(10000);
+  this.timeout(15000);
+
+  beforeEach(async () => {
+    testClient = new TestClient(applicationArgs);
+
+    await testClient.startSpectronApp();
+    await testClient.awaitReadyness();
+  });
+
+  afterEach(async () => {
+    if (await testClient.isSpectronAppRunning()) {
+      await testClient.stopSpectronApp();
+      return testClient.clearDatabase();
+    }
+    return null;
+  });
+
+  this.afterAll(async () => {
+    await testClient.clearSavedDiagrams();
+  });
+
+  it('should open a solution', async () => {
+    await testClient.startPageLoaded();
+
+    await testClient.solutionExplorer.openDirectoryAsSolution('fixtures');
+  });
+
+  it('should open a diagram from solution', async () => {
+    await testClient.startPageLoaded();
+
+    const diagramName = 'call_activity_subprocess_error';
+    await testClient.solutionExplorer.openDirectoryAsSolution('fixtures', diagramName);
+    await testClient.assertTitleIs(diagramName);
+  });
+
+  it('should open the internal ProcessEngine as solution', async () => {
+    await testClient.startPageLoaded();
+
+    await testClient.solutionExplorer.assertInternalProcessEngineIsOpenedAsSolution();
+  });
+
+  it('should show the SolutionExplorer', async () => {
+    // Arrange
+    await testClient.startPageLoaded();
+    await testClient.solutionExplorer.hide();
+
+    // Act and Assert
+    await testClient.solutionExplorer.show();
+  });
+
+  it('should hide the SolutionExplorer', async () => {
+    await testClient.startPageLoaded();
+
+    await testClient.solutionExplorer.hide();
+  });
+});
