292a2b7bea2fd5c0c2d19583987c5f16a131f82d
Alex😎 <alexander.kasten@5minds.de>
2019-09-06 15:41:56 +0200

    :recycle: Refactor design test

--- test/integration/design.spec.ts
+++ test/integration/design.spec.ts
@@ -1,16 +1,7 @@
 import {TestClient} from './TestClient';
-import {applicationArgs} from './get-application-args';
-
-async function createAndOpenDiagram(): Promise<void> {
-  if (!creatingFirstDiagram) {
-    await testClient.openStartPage();
-  }
-
-  await testClient.createAndOpenNewDiagram();
-}
+import {applicationArgs} from './modules/get-application-args';
 
 let testClient: TestClient;
-let creatingFirstDiagram: boolean = true;
 
 describe('Design View', function foo() {
   this.slow(10000);
@@ -19,7 +10,7 @@ describe('Design View', function foo() {
   beforeEach(async () => {
     testClient = new TestClient(applicationArgs);
 
-    creatingFirstDiagram = true;
+    testClient.creatingFirstDiagram = true;
     await testClient.startSpectronApp();
     await testClient.awaitReadyness();
   });
@@ -38,7 +29,7 @@ describe('Design View', function foo() {
 
   it('should save a diagram', async () => {
     // Arrange
-    await createAndOpenDiagram();
+    await testClient.createAndOpenNewDiagram();
     await testClient.assertDiagramIsUnsaved();
 
     // Act and Assert
@@ -60,7 +51,7 @@ describe('Design View', function foo() {
     // Act
     await testClient.designView.deployDiagram();
     // Assert
-    await testClient.assertTitleIs(diagramName);
+    await testClient.assertNavbarTitleIs(diagramName);
     await testClient.assertDiagramIsOnProcessEngine();
   });
 
@@ -70,33 +61,48 @@ describe('Design View', function foo() {
     await testClient.solutionExplorer.openDirectoryAsSolution('fixtures', diagramName);
     await testClient.assertDiagramIsOnFileSystem();
     await testClient.designView.deployDiagram();
-    await testClient.assertTitleIs(diagramName);
+    await testClient.assertNavbarTitleIs(diagramName);
     await testClient.assertDiagramIsOnProcessEngine();
 
     await testClient.designView.startProcess();
   });
 
   it('should render a diagram correctly', async () => {
-    await createAndOpenDiagram();
+    await testClient.createAndOpenNewDiagram();
 
     await testClient.ensureVisible('[data-element-id="Collaboration_1cidyxu"]');
   });
 
   it('should select StartEvent after opening a diagram', async () => {
-    await createAndOpenDiagram();
-    await testClient.showPropertyPanel();
+    await testClient.createAndOpenNewDiagram();
+    await testClient.designView.showPropertyPanel();
 
     await testClient.assertSelectedBpmnElementHasName('Start Event');
   });
 
   it('should select element on click', async () => {
-    await createAndOpenDiagram();
-    await testClient.showPropertyPanel();
+    await testClient.createAndOpenNewDiagram();
+    await testClient.designView.showPropertyPanel();
 
     const elementName = 'End Event';
 
     await testClient.rejectSelectedBpmnElementHasName(elementName);
     await testClient.clickOnBpmnElementWithName(elementName);
     await testClient.assertSelectedBpmnElementHasName(elementName);
+  });
+
+  it('should open the xml view from the status bar', async () => {
+    await testClient.createAndOpenNewDiagram();
+    await testClient.designView.openXmlViewFromStatusbar();
+    await testClient.designView.assertXmlViewIsVisible();
+  });
+
+  it('should open design view from the status bar', async () => {
+    // Arrange
+    await testClient.createAndOpenNewDiagram();
+    await testClient.designView.openXmlViewFromStatusbar();
+
+    // Act and Assert
+    await testClient.designView.openDetailViewFromStatusbar();
   });
 });
