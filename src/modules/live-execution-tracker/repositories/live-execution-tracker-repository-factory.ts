697f903738315a26d640fc57f0bc4cb640f0002d
Steffen <Steffen.knaup@5minds.de>
2019-09-19 11:42:57 +0200

    :lipstick: Add Missing Type

--- src/modules/live-execution-tracker/repositories/live-execution-tracker-repository-factory.ts
+++ src/modules/live-execution-tracker/repositories/live-execution-tracker-repository-factory.ts
@@ -1,9 +1,15 @@
+import {IManagementApiClient} from '@process-engine/management_api_contracts';
+
 import {ILiveExecutionTrackerRepository} from '../contracts/index';
+
 import {processEngineSupportsPagination} from '../../../services/process-engine-version-module/process-engine-version-module';
 import {LiveExecutionTrackerPaginationRepository} from './live-execution-tracker.pagination-repository';
 import {LiveExecutionTrackerRepository} from './live-execution-tracker.repository';
 
-export function createRepository(managementApiClient, runtimeVersion: string): ILiveExecutionTrackerRepository {
+export function createRepository(
+  managementApiClient: IManagementApiClient,
+  runtimeVersion: string,
+): ILiveExecutionTrackerRepository {
   if (processEngineSupportsPagination(runtimeVersion)) {
     return new LiveExecutionTrackerPaginationRepository(managementApiClient);
   }
