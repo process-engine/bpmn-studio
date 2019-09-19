8b191b8017170669e8c1bb4cb42c8286d509e7f8
Alex😎 <alexander.kasten@5minds.de>
2019-09-19 09:45:12 +0200

    :sparkles: Adjust the contracts for pagination

--- src/modules/inspect/inspect-correlation/contracts/IInspect-correlation.repository.ts
+++ src/modules/inspect/inspect-correlation/contracts/IInspect-correlation.repository.ts
@@ -5,47 +5,31 @@ export interface IInspectCorrelationRepository {
   getAllCorrelationsForProcessModelId(
     processModelId: string,
     identity: IIdentity,
+    offset?: number,
+    limit?: number,
   ): Promise<DataModels.Correlations.CorrelationList>;
   getLogsForCorrelation(
     correlation: DataModels.Correlations.Correlation,
     identity: IIdentity,
+    offset?: number,
+    limit?: number,
   ): Promise<Array<DataModels.Logging.LogEntryList>>;
   getLogsForProcessInstance(
     processModelId: string,
     processInstance: string,
     identity: IIdentity,
+    offset?: number,
+    limit?: number,
   ): Promise<DataModels.Logging.LogEntryList>;
   getTokenForFlowNodeInstance(
     processModelId: string,
     correlationId: string,
     flowNodeId: string,
     identity: IIdentity,
+    offset?: number,
+    limit?: number,
   ): Promise<DataModels.TokenHistory.TokenHistoryEntryList>;
   getTokenForFlowNodeByProcessInstanceId(
-    processInstanceId: string,
-    flowNodeId: string,
-    identity: IIdentity,
-  ): Promise<DataModels.TokenHistory.TokenHistoryGroup>;
-  getOldAllCorrelationsForProcessModelId(
-    processModelId: string,
-    identity: IIdentity,
-  ): Promise<Array<DataModels.Correlations.Correlation>>;
-  getOldLogsForCorrelation(
-    correlation: DataModels.Correlations.Correlation,
-    identity: IIdentity,
-  ): Promise<Array<DataModels.Logging.LogEntry>>;
-  getOldLogsForProcessInstance(
-    processModelId: string,
-    processInstance: string,
-    identity: IIdentity,
-  ): Promise<Array<DataModels.Logging.LogEntry>>;
-  getOldTokenForFlowNodeInstance(
-    processModelId: string,
-    correlationId: string,
-    flowNodeId: string,
-    identity: IIdentity,
-  ): Promise<Array<DataModels.TokenHistory.TokenHistoryEntry>>;
-  getOldTokenForFlowNodeByProcessInstanceId(
     processInstanceId: string,
     flowNodeId: string,
     identity: IIdentity,
