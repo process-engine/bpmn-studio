4beef273d4ea9c4d7635744c80b277b6798e7aea
Steffen <Steffen.knaup@5minds.de>
2019-09-19 10:40:37 +0200

    :bug: Fix Creating InspectCorrelationService

--- src/modules/inspect/inspect-correlation/index.ts
+++ src/modules/inspect/inspect-correlation/index.ts
@@ -1,7 +1,18 @@
 import {FrameworkConfiguration} from 'aurelia-framework';
+import {EventAggregator} from 'aurelia-event-aggregator';
+
+import {IManagementApiClient} from '@process-engine/management_api_contracts';
 
 import {InspectCorrelationService} from './services/inspect-correlation.service';
 
 export function configure(config: FrameworkConfiguration): void {
-  config.container.registerSingleton('InspectCorrelationService', InspectCorrelationService);
+  const eventAggregator: EventAggregator = config.container.get(EventAggregator);
+  const managementApiClient: IManagementApiClient = config.container.get('ManagementApiClientService');
+
+  const inspectCorrelationService: InspectCorrelationService = new InspectCorrelationService(
+    eventAggregator,
+    managementApiClient,
+  );
+
+  config.container.registerInstance('InspectCorrelationService', inspectCorrelationService);
 }
