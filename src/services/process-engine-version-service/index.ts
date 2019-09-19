671342ded1f2bfbe805365d0d1f60b2c2deb90da
Alex😎 <alexander.kasten@5minds.de>
2019-09-19 09:44:31 +0200

    :fire: Remove process engine version service

--- src/services/process-engine-version-service/index.ts
+++
@@ -1,6 +0,0 @@
-import {FrameworkConfiguration} from 'aurelia-framework';
-import {ProcessEngineVersionService} from './process-engine-version-service';
-
-export function configure(config: FrameworkConfiguration): void {
-  config.container.registerSingleton('ProcessEngineVersionService', ProcessEngineVersionService);
-}
