f7e27e1a89a3bc60d598ed12ed65640afab9678e
Alex😎 <alexander.kasten@5minds.de>
2019-09-19 09:46:24 +0200

    :sparkles: Add check for using the correct repository to the service

--- src/modules/inspect/inspect-correlation/services/inspect-correlation.service.ts
+++ src/modules/inspect/inspect-correlation/services/inspect-correlation.service.ts
@@ -1,54 +1,51 @@
 import {inject} from 'aurelia-framework';
 
 import {IIdentity} from '@essential-projects/iam_contracts';
-import {DataModels} from '@process-engine/management_api_contracts';
+import {DataModels, IManagementApiClient} from '@process-engine/management_api_contracts';
 
+import {EventAggregator} from 'aurelia-event-aggregator';
 import {IInspectCorrelationRepository, IInspectCorrelationService} from '../contracts';
-import {ProcessEngineVersionService} from '../../../../services/process-engine-version-service/process-engine-version-service';
+import {InspectCorrelationPaginationRepository} from '../repositories/inspect-correlation.pagination.repository';
+import environment from '../../../../environment';
+import {InspectCorrelationRepository} from '../repositories/inspect-correlation.repository';
+import {ISolutionEntry} from '../../../../contracts';
+import {supportsPaginationAndNewDataModels} from '../../../../services/process-engine-version-module/process-engine-version-module';
 
-@inject('InspectCorrelationRepository', 'ProcessEngineVersionService')
+@inject(EventAggregator, 'ManagementApiClientService')
 export class InspectCorrelationService implements IInspectCorrelationService {
   private inspectCorrelationRepository: IInspectCorrelationRepository;
-  private processEngineVersionService: ProcessEngineVersionService;
+  private eventAggregator: EventAggregator;
+  private managementApiService: IManagementApiClient;
 
-  constructor(
-    inspectCorrelationRepository: IInspectCorrelationRepository,
-    processEngineVersionService: ProcessEngineVersionService,
-  ) {
-    this.inspectCorrelationRepository = inspectCorrelationRepository;
-    this.processEngineVersionService = processEngineVersionService;
+  constructor(eventAggregator: EventAggregator, managementApiService: IManagementApiClient) {
+    this.eventAggregator = eventAggregator;
+    this.managementApiService = managementApiService;
+
+    this.eventAggregator.subscribe(
+      environment.events.configPanel.solutionEntryChanged,
+      (solutionEntry: ISolutionEntry) => {
+        if (supportsPaginationAndNewDataModels(solutionEntry)) {
+          this.inspectCorrelationRepository = new InspectCorrelationPaginationRepository(this.managementApiService);
+        } else {
+          this.inspectCorrelationRepository = new InspectCorrelationRepository(this.managementApiService);
+        }
+
+      },
+    );
   }
 
   public async getAllCorrelationsForProcessModelId(
     processModelId: string,
     identity: IIdentity,
   ): Promise<DataModels.Correlations.CorrelationList> {
-    const supportsPaginationAndNewDataModels: boolean = this.processEngineVersionService.supportsPaginationAndNewDataModels();
-    console.log(supportsPaginationAndNewDataModels);
-
-    if (supportsPaginationAndNewDataModels) {
-      return this.inspectCorrelationRepository.getAllCorrelationsForProcessModelId(processModelId, identity);
-    }
-
-    const result = await this.inspectCorrelationRepository.getOldAllCorrelationsForProcessModelId(
-      processModelId,
-      identity,
-    );
-    return {correlations: result, totalCount: result.length};
+    return this.inspectCorrelationRepository.getAllCorrelationsForProcessModelId(processModelId, identity);
   }
 
   public async getLogsForCorrelation(
     correlation: DataModels.Correlations.Correlation,
     identity: IIdentity,
   ): Promise<Array<DataModels.Logging.LogEntryList>> {
-    const supportsPaginationAndNewDataModels: boolean = this.processEngineVersionService.supportsPaginationAndNewDataModels();
-    if (supportsPaginationAndNewDataModels) {
-      return this.inspectCorrelationRepository.getLogsForCorrelation(correlation, identity);
-    }
-
-    const result = await this.inspectCorrelationRepository.getOldLogsForCorrelation(correlation, identity);
-
-    return [{logEntries: result, totalCount: result.length}];
+    return this.inspectCorrelationRepository.getLogsForCorrelation(correlation, identity);
   }
 
   public async getLogsForProcessInstance(
@@ -56,18 +53,7 @@ export class InspectCorrelationService implements IInspectCorrelationService {
     processInstanceId: string,
     identity: IIdentity,
   ): Promise<DataModels.Logging.LogEntryList> {
-    const supportsPaginationAndNewDataModels: boolean = this.processEngineVersionService.supportsPaginationAndNewDataModels();
-    if (supportsPaginationAndNewDataModels) {
-      return this.inspectCorrelationRepository.getLogsForProcessInstance(processModelId, processInstanceId, identity);
-    }
-
-    const result = await this.inspectCorrelationRepository.getOldLogsForProcessInstance(
-      processModelId,
-      processInstanceId,
-      identity,
-    );
-
-    return {logEntries: result, totalCount: result.length};
+    return this.inspectCorrelationRepository.getLogsForProcessInstance(processModelId, processInstanceId, identity);
   }
 
   public async getTokenForFlowNodeInstance(
@@ -77,35 +63,15 @@ export class InspectCorrelationService implements IInspectCorrelationService {
     identity: IIdentity,
   ): Promise<DataModels.TokenHistory.TokenHistoryGroup | undefined> {
     try {
-      const supportsPaginationAndNewDataModels: boolean = this.processEngineVersionService.supportsPaginationAndNewDataModels();
-
-      if (supportsPaginationAndNewDataModels) {
-        const tokenHistory: DataModels.TokenHistory.TokenHistoryGroup = {};
-        const tokenForFlowNodeInstance: DataModels.TokenHistory.TokenHistoryEntryList = await this.inspectCorrelationRepository.getTokenForFlowNodeInstance(
-          processModelId,
-          correlationId,
-          flowNodeId,
-          identity,
-        );
-
-        tokenHistory[tokenForFlowNodeInstance[0].flowNodeId] = tokenForFlowNodeInstance;
-        return tokenHistory;
-      }
-
       const tokenHistory: DataModels.TokenHistory.TokenHistoryGroup = {};
-      const tokenForFlowNodeInstance: Array<
-        DataModels.TokenHistory.TokenHistoryEntry
-      > = await this.inspectCorrelationRepository.getOldTokenForFlowNodeInstance(
+      const tokenForFlowNodeInstance: DataModels.TokenHistory.TokenHistoryEntryList = await this.inspectCorrelationRepository.getTokenForFlowNodeInstance(
         processModelId,
         correlationId,
         flowNodeId,
         identity,
       );
 
-      tokenHistory[tokenForFlowNodeInstance[0].flowNodeId] = {
-        tokenHistoryEntries: tokenForFlowNodeInstance,
-        totalCount: tokenForFlowNodeInstance.length,
-      };
+      tokenHistory[tokenForFlowNodeInstance[0].flowNodeId] = tokenForFlowNodeInstance;
       return tokenHistory;
     } catch (error) {
       return undefined;
@@ -118,17 +84,7 @@ export class InspectCorrelationService implements IInspectCorrelationService {
     identity: IIdentity,
   ): Promise<DataModels.TokenHistory.TokenHistoryGroup | undefined> {
     try {
-      const supportsPaginationAndNewDataModels: boolean = this.processEngineVersionService.supportsPaginationAndNewDataModels();
-
-      if (supportsPaginationAndNewDataModels) {
-        return await this.inspectCorrelationRepository.getTokenForFlowNodeByProcessInstanceId(
-          processInstanceId,
-          flowNodeId,
-          identity,
-        );
-      }
-
-      return await this.inspectCorrelationRepository.getOldTokenForFlowNodeByProcessInstanceId(
+      return await this.inspectCorrelationRepository.getTokenForFlowNodeByProcessInstanceId(
         processInstanceId,
         flowNodeId,
         identity,
