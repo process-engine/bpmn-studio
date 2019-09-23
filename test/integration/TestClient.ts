2a72c55d920a5f36a935b93d3ae438bc9077af6e
Alex😎 <alexander.kasten@5minds.de>
2019-09-23 12:36:21 +0200

    :bug: Fix typo

--- test/integration/TestClient.ts
+++ test/integration/TestClient.ts
@@ -125,7 +125,7 @@ export class TestClient {
       const files = fs.readdirSync(SAVE_DIAGRAM_DIR, {encoding: 'utf8'});
       files.forEach((file: string) => {
         console.log(file);
-        fs.unlinkSync(`${DATABASE_PATH}/${file}`);
+        fs.unlinkSync(`${SAVE_DIAGRAM_DIR}/${file}`);
       });
       fs.rmdirSync(SAVE_DIAGRAM_DIR);
     }
