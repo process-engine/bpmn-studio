08997b61e5aa258f12770334c3c1e7aa31f232f7
Alex😎 <alexander.kasten@5minds.de>
2019-09-26 16:56:50 +0200

    :sparkles: Add inspect correlation processinstance query repository

---
+++ src/modules/inspect/inspect-correlation/repositories/inspect-correlation.process-instances.queries.repository.ts
@@ -0,0 +1,16 @@
+import {ProcessInstanceList} from '@process-engine/management_api_contracts/dist/data_models/correlation';
+import {IIdentity} from '@essential-projects/iam_contracts';
+import {InspectCorrelationPaginationRepository} from './inspect-correlation.pagination.repository';
+import {IInspectCorrelationRepository} from '../contracts';
+
+export class InspectCorrelationProcessInstancesQueryRepository extends InspectCorrelationPaginationRepository
+  implements IInspectCorrelationRepository {
+  public getProcessInstancesForProcessModel(
+    identity: IIdentity,
+    processModelId: string,
+    offset?: number,
+    limit?: number,
+  ): Promise<ProcessInstanceList> {
+    return this.managementApiService.getProcessInstancesForProcessModel(identity, processModelId, offset, limit);
+  }
+}
