a5ec6f60a370a868ab7047a61b92e35b41c870ce
Steffen <Steffen.knaup@5minds.de>
2019-09-27 08:33:32 +0200

    :zap: Improve Performance of Apply Pagination

--- src/services/pagination-module/pagination-module.ts
+++ src/services/pagination-module/pagination-module.ts
@@ -1,13 +1,6 @@
 export function applyPagination<TList>(list: Array<TList>, offset: number, limit: number): Array<TList> {
-  const paginatedList: Array<TList> = list.slice();
+  const startValue: number = Math.max(0, offset);
+  const endValue: number = limit > 0 ? Math.min(offset + limit, list.length + 1) : list.length + 1;
 
-  if (offset > 0) {
-    paginatedList.splice(0, offset);
-  }
-
-  if (limit > 0) {
-    paginatedList.splice(limit, paginatedList.length - limit);
-  }
-
-  return paginatedList;
+  return list.slice(startValue, endValue);
 }
