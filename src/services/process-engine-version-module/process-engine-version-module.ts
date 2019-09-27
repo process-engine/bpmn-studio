807537c950625e32f665f76fefb39b70aa958f69
Steffen <Steffen.knaup@5minds.de>
2019-09-27 14:24:33 +0200

    :sparkles: Add 'SupportsGettingProcessInstacnes' Check

--- src/services/process-engine-version-module/process-engine-version-module.ts
+++ src/services/process-engine-version-module/process-engine-version-module.ts
@@ -23,3 +23,27 @@ export function processEngineSupportsPagination(processEngineVersion: string): b
 
   return solutionEntryPEVersion.compare(stableVersionWithPagination) >= 0;
 }
+
+export function processEngineSupportsGettingProcessInstances(processEngineVersion: string): boolean {
+  if (!processEngineVersion) {
+    return undefined;
+  }
+
+  const solutionEntryPEVersion = new SemVer(processEngineVersion);
+
+  const alphaVersionWithProcessInstanceFunctions = new SemVer('9.0.0-alpha.10');
+  const betaVersionWithProcessInstanceFunctions = new SemVer('9.0.0-beta.7');
+  const stableVersionWithProcessInstanceFunctions = new SemVer('9.0.0');
+
+  const solutionEntryIsAlpha: boolean = solutionEntryPEVersion.prerelease[0] === 'alpha';
+  const solutionEntryIsBeta: boolean = solutionEntryPEVersion.prerelease[0] === 'beta';
+
+  if (solutionEntryIsAlpha) {
+    return solutionEntryPEVersion.compare(alphaVersionWithProcessInstanceFunctions) >= 0;
+  }
+  if (solutionEntryIsBeta) {
+    return solutionEntryPEVersion.compare(betaVersionWithProcessInstanceFunctions) >= 0;
+  }
+
+  return solutionEntryPEVersion.compare(stableVersionWithProcessInstanceFunctions) >= 0;
+}
