4b43dd97f5c84f3789f9a1c5d9322cbfd4a43657
Steffen <Steffen.knaup@5minds.de>
2019-09-30 15:51:33 +0200

    :bug: Fix EndValue Calculation in 'applyPagination'

--- src/services/pagination-module/pagination-module.ts
+++ src/services/pagination-module/pagination-module.ts
@@ -1,6 +1,5 @@
 export function applyPagination<TList>(list: Array<TList>, offset: number, limit: number): Array<TList> {
-  const startValue: number = Math.max(0, offset);
-  const endValue: number = limit > 0 ? Math.min(offset + limit, list.length + 1) : list.length + 1;
+  const endValue: number = limit > 0 ? Math.min(offset + limit, list.length) : list.length;
 
-  return list.slice(startValue, endValue);
+  return list.slice(offset, endValue);
 }
