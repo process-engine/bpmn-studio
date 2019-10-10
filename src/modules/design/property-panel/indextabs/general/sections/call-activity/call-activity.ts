138c09e7d1256054bd5ceb28813f3258b8447ff6
Alex 😎 <alexander.kasten@5minds.de>
2019-10-10 16:49:46 +0200

    :construction: WIP Allow to select startevents of callactivity

--- src/modules/design/property-panel/indextabs/general/sections/call-activity/call-activity.ts
+++ src/modules/design/property-panel/indextabs/general/sections/call-activity/call-activity.ts
@@ -1,11 +1,18 @@
 import {EventAggregator} from 'aurelia-event-aggregator';
-import {inject} from 'aurelia-framework';
+import {inject, observable} from 'aurelia-framework';
 import {Router} from 'aurelia-router';
 
-import {ICallActivityElement, IShape} from '@process-engine/bpmn-elements_contracts';
+import {
+  ICallActivityElement,
+  IExtensionElement,
+  IModdleElement,
+  IPropertiesElement,
+  IProperty,
+  IShape,
+} from '@process-engine/bpmn-elements_contracts';
 import {IDiagram} from '@process-engine/solutionexplorer.contracts';
 
-import {IPageModel, ISection} from '../../../../../../../contracts';
+import {IBpmnModdle, IPageModel, ISection} from '../../../../../../../contracts';
 import environment from '../../../../../../../environment';
 import {GeneralService} from '../../service/general.service';
 
@@ -14,7 +21,9 @@ export class CallActivitySection implements ISection {
   public path: string = '/sections/call-activity/call-activity';
   public canHandleElement: boolean = false;
   public allDiagrams: Array<IDiagram>;
+  public startEvents: Array<IShape>;
   public selectValue: string;
+  @observable public selectedStartEvent: string;
   public selectedDiagramName: string;
 
   public previouslySelectedDiagram: string;
@@ -24,6 +33,7 @@ export class CallActivitySection implements ISection {
   private router: Router;
   private eventAggregator: EventAggregator;
   private activeSolutionUri: string;
+  private moddle: IBpmnModdle;
 
   constructor(generalService?: GeneralService, router?: Router, eventAggregator?: EventAggregator) {
     this.generalService = generalService;
@@ -32,13 +42,24 @@ export class CallActivitySection implements ISection {
   }
 
   public async activate(model: IPageModel): Promise<void> {
+    this.moddle = model.modeler.get('moddle');
     this.activeSolutionUri = this.router.currentInstruction.queryParams.solutionUri;
     this.businessObjInPanel = model.elementInPanel.businessObject;
 
+    console.log(this.businessObjInPanel);
     await this.getAllDiagrams();
 
     this.previouslySelectedDiagram = this.businessObjInPanel.calledElement;
     this.selectedDiagramName = this.businessObjInPanel.calledElement;
+    this.selectValue = this.selectedDiagramName;
+
+    if (this.selectValue) {
+      this.startEvents = await this.generalService.getAllStartEventsForDiagram(this.selectValue);
+
+      const previousSelectedStartEvent = this.getSelectedStartEvent();
+
+      this.selectedStartEvent = previousSelectedStartEvent || this.startEvents[0].id;
+    }
   }
 
   public isSuitableForElement(element: IShape): boolean {
@@ -64,16 +85,102 @@ export class CallActivitySection implements ISection {
     });
   }
 
-  public updateCalledDiagram(): void {
+  public selectedStartEventChanged(newValue, oldValue): void {
+    console.log(newValue, oldValue);
+
+    if (newValue === undefined) {
+      delete this.businessObjInPanel.extensionElements;
+      console.log(this.businessObjInPanel);
+
+      return;
+    }
+
+    const noExtensionsElements =
+      this.businessObjInPanel.extensionElements === undefined ||
+      this.businessObjInPanel.extensionElements.values === undefined ||
+      this.businessObjInPanel.extensionElements.values.length === 0;
+
+    if (noExtensionsElements) {
+      this.createExtensionElement();
+    }
+
+    const bpmnPropertyProperties: object = {
+      name: 'startEventId',
+      value: newValue,
+    };
+    const bpmnProperty: IProperty = this.moddle.create('camunda:Property', bpmnPropertyProperties);
+
+    const propertiesElement = this.getPropertiesElement();
+
+    const startEventProperty = propertiesElement.values.findIndex((value: IProperty) => value.name === 'startEventId');
+
+    if (startEventProperty >= 0) {
+      propertiesElement.values.splice(startEventProperty, 1);
+    }
+
+    propertiesElement.values.push(bpmnProperty);
+
+    console.log(this.businessObjInPanel);
+    this.publishDiagramChange();
+  }
+
+  public async updateCalledDiagram(): Promise<void> {
     const diagramSelectedViaSelect: boolean = this.selectValue !== undefined;
     if (diagramSelectedViaSelect) {
       this.selectedDiagramName = this.selectValue;
       this.selectValue = undefined;
+      this.startEvents = await this.generalService.getAllStartEventsForDiagram(this.selectedDiagramName);
     }
 
     this.businessObjInPanel.calledElement = this.selectedDiagramName;
 
     this.publishDiagramChange();
+  }
+
+  private getPropertiesElement(): IPropertiesElement {
+    const propertiesElement: IPropertiesElement = this.businessObjInPanel.extensionElements.values.find(
+      (extensionValue: IExtensionElement) => {
+        if (!extensionValue) {
+          return undefined;
+        }
+
+        const extensionIsPropertiesElement: boolean =
+          extensionValue.$type === 'camunda:Properties' &&
+          extensionValue.values !== undefined &&
+          extensionValue.values !== null;
+
+        return extensionIsPropertiesElement;
+      },
+    );
+
+    return propertiesElement;
+  }
+
+  private createExtensionElement(): void {
+    const extensionValues: Array<IModdleElement> = [];
+    const properties: Array<IProperty> = [];
+    const propertiesElement: IPropertiesElement = this.moddle.create('camunda:Properties', {values: properties});
+    extensionValues.push(propertiesElement);
+
+    const extensionElements: IModdleElement = this.moddle.create('bpmn:ExtensionElements', {
+      values: extensionValues,
+    });
+    this.businessObjInPanel.extensionElements = extensionElements;
+  }
+
+  private getSelectedStartEvent(): string | undefined {
+    const extensionElementAndPropertiesExist =
+      this.businessObjInPanel.extensionElements !== undefined &&
+      this.businessObjInPanel.extensionElements.values !== undefined &&
+      this.businessObjInPanel.extensionElements.values.length !== 0;
+
+    if (!extensionElementAndPropertiesExist) {
+      return undefined;
+    }
+
+    const propertiesElement = this.getPropertiesElement();
+
+    return propertiesElement.values.find((value: IPropertiesElement) => value.name === 'startEventId').value;
   }
 
   private async getAllDiagrams(): Promise<void> {
