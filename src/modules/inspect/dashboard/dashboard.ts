568212e7f7bc9030590ce128cfb6af9d857d03e0
Steffen <Steffen.knaup@5minds.de>
2019-11-08 10:03:21 +0100

    :recycle: Use ProcessEngineVersionModule for Support Checks

--- src/modules/inspect/dashboard/dashboard.ts
+++ src/modules/inspect/dashboard/dashboard.ts
@@ -1,34 +1,22 @@
 import {bindable} from 'aurelia-framework';
 import {ISolutionEntry} from '../../../contracts';
+import {processEngineSupportsCronjobs} from '../../../services/process-engine-version-module/process-engine-version.module';
 
-const versionRegex: RegExp = /(\d+)\.(\d+).(\d+)/;
 export class Dashboard {
   @bindable() public activeSolutionEntry: ISolutionEntry;
   public showCronjobList: boolean = false;
 
   public attached(): void {
-    this.showCronjobList = this.processEngineSupportsCronjob();
+    const processEngineVersion: string = this.activeSolutionEntry.processEngineVersion;
+    const activeSolutionHasVersion: boolean = processEngineVersion !== undefined;
+
+    this.showCronjobList = activeSolutionHasVersion ? processEngineSupportsCronjobs(processEngineVersion) : false;
   }
 
   public activeSolutionEntryChanged(): void {
-    this.showCronjobList = this.processEngineSupportsCronjob();
-  }
+    const processEngineVersion = this.activeSolutionEntry.processEngineVersion;
+    const activeSolutionHasVersion: boolean = processEngineVersion !== undefined;
 
-  private processEngineSupportsCronjob(): boolean {
-    const processEngineVersion: string = this.activeSolutionEntry.processEngineVersion;
-
-    const noProcessEngineVersionSet: boolean = processEngineVersion === undefined;
-    if (noProcessEngineVersionSet) {
-      return false;
-    }
-
-    const regexResult: RegExpExecArray = versionRegex.exec(processEngineVersion);
-    const majorVersion: number = parseInt(regexResult[1]);
-    const minorVersion: number = parseInt(regexResult[2]);
-
-    // The version must be 8.4.0 or later
-    const processEngineSupportsEvents: boolean = majorVersion > 8 || (majorVersion === 8 && minorVersion >= 4);
-
-    return processEngineSupportsEvents;
+    this.showCronjobList = activeSolutionHasVersion ? processEngineSupportsCronjobs(processEngineVersion) : false;
   }
 }
