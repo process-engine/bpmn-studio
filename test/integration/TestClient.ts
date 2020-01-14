63e6946a9287d69fb59e6bc7b9f6f402ad46a097
Alex 😎 <alexander.kasten@5minds.de>
2020-01-14 16:35:23 +0100

    :fire: Remove unneeded browserWindow getter

--- test/integration/TestClient.ts
+++ test/integration/TestClient.ts
@@ -1,4 +1,3 @@
-import {BrowserWindow} from 'electron';
 /* eslint-disable no-useless-escape */
 /* eslint-disable no-empty-function */
 import path from 'path';
@@ -140,10 +139,6 @@ export class TestClient {
 
   public get webdriverClient(): any {
     return this.app.client;
-  }
-
-  public get browserWindow(): BrowserWindow {
-    return this.app.browserWindow;
   }
 
   public async clickOn(selector: string): Promise<any> {
