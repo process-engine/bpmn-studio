a6e8f7bc4f1b0a9c6bd93fc54e20266396ece882
Steffen <Steffen.knaup@5minds.de>
2020-01-16 12:56:06 +0100

    :fire: Remove DownloadJS

--- src/modules/design/bpmn-io/repositories/diagram-export.repository.ts
+++ src/modules/design/bpmn-io/repositories/diagram-export.repository.ts
@@ -1,8 +1,7 @@
-import download from 'downloadjs';
 import {IDiagramExportRepositoryContracts} from '../../../../contracts/exportRepository';
 
 export class DiagramExportRepository implements IDiagramExportRepositoryContracts {
   public exportDiagram(fileContent: string, outputName: string, mimeType: string): void {
-    download(fileContent, outputName, mimeType);
+    // download(fileContent, outputName, mimeType);
   }
 }
