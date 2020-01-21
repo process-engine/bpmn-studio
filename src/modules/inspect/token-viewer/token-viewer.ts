4395446af079249870e4732015c5ac390da2da3c
Alex 😎 <alexander.kasten@5minds.de>
2020-01-21 16:24:11 +0100

    :bug: Fix bug where the Tokenviewer crashes if an external task returns null
    Fixes #1946

--- src/modules/inspect/token-viewer/token-viewer.ts
+++ src/modules/inspect/token-viewer/token-viewer.ts
@@ -224,7 +224,7 @@ export class TokenViewer {
   private convertHistoryEntryPayloadToTokenEntryPayload(tokenEntryPayload: any): Array<IPayloadEntry> {
     const formattedTokenEntryPayload: Array<IPayloadEntry> = [];
 
-    const payloadIsNotAnObjectOrArray: boolean = typeof tokenEntryPayload !== 'object';
+    const payloadIsNotAnObjectOrArray: boolean = typeof tokenEntryPayload !== 'object' || tokenEntryPayload === null;
     if (payloadIsNotAnObjectOrArray) {
       const payloadEntry: IPayloadEntry = this.getPayloadEntryForNonObject(tokenEntryPayload);
 
@@ -303,9 +303,9 @@ export class TokenViewer {
   }
 
   private getPayloadEntryValuesForNonObject(payload: any): Array<IPayloadEntryValue> {
-    const payloadIsString: boolean = typeof payload === 'string';
+    const payloadIsStringOrNull: boolean = typeof payload === 'string' || payload === null;
 
-    const payloadEntryValue: string = payloadIsString ? `"${payload}"` : payload.toString();
+    const payloadEntryValue: string = payloadIsStringOrNull ? `"${payload}"` : payload.toString();
 
     const payloadEntryValues: Array<IPayloadEntryValue> = [{value: payloadEntryValue}];
 
