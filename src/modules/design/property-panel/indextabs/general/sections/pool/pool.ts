21b7ade1c306a824c44b051400d0d25fe191d794
Steffen <Steffen.knaup@5minds.de>
2020-01-22 12:30:57 +0100

    :bug: Fix Checking the Uniqueness of the Ids of RootElements When Changing ProcessId

--- src/modules/design/property-panel/indextabs/general/sections/pool/pool.ts
+++ src/modules/design/property-panel/indextabs/general/sections/pool/pool.ts
@@ -1,5 +1,5 @@
 import {EventAggregator} from 'aurelia-event-aggregator';
-import {bindable, inject} from 'aurelia-framework';
+import {bindable, BindingEngine, Disposable inject} from 'aurelia-framework';
 import {ValidateEvent, ValidationController, ValidationRules} from 'aurelia-validation';
 
 import {IModdleElement, IPoolElement, IShape} from '@process-engine/bpmn-elements_contracts';
@@ -7,13 +7,14 @@ import {IModdleElement, IPoolElement, IShape} from '@process-engine/bpmn-element
 import {IBpmnModeler, IElementRegistry, IPageModel, ISection} from '../../../../../../../contracts';
 import environment from '../../../../../../../environment';
 
-@inject(ValidationController, EventAggregator)
+@inject(ValidationController, EventAggregator, BindingEngine)
 export class PoolSection implements ISection {
   public path: string = '/sections/pool/pool';
   public canHandleElement: boolean = false;
   public validationController: ValidationController;
   public validationError: boolean = false;
   public businessObjInPanel: IPoolElement;
+  public processRefId: string;
   @bindable public processIdCheckboxChecked: boolean = false;
   public showModal: boolean = false;
   public showProcessIdWarningModal: boolean = false;
@@ -21,10 +22,13 @@ export class PoolSection implements ISection {
   private modeler: IBpmnModeler;
   private previousProcessRefId: string;
   private eventAggregator: EventAggregator;
+  private bindingEngine: BindingEngine;
+  private processRefIdObserver: Disposable;
 
-  constructor(controller?: ValidationController, eventAggregator?: EventAggregator) {
+  constructor(controller?: ValidationController, eventAggregator?: EventAggregator, bindingEngine?: BindingEngine) {
     this.validationController = controller;
     this.eventAggregator = eventAggregator;
+    this.bindingEngine = bindingEngine;
   }
 
   public activate(model: IPageModel): void {
@@ -34,14 +38,26 @@ export class PoolSection implements ISection {
     }
 
     if (this.validationError) {
-      this.businessObjInPanel.processRef.id = this.previousProcessRefId;
+      this.processRefId = this.previousProcessRefId;
       this.validationController.validate();
     }
 
     this.businessObjInPanel = model.elementInPanel.businessObject;
+    this.processRefId = this.businessObjInPanel.processRef.id;
     this.previousProcessRefId = this.businessObjInPanel.processRef.id;
 
     this.modeler = model.modeler;
+
+    if (this.processRefIdObserver !== undefined) {
+      this.processRefIdObserver.dispose();
+      this.processRefIdObserver = undefined;
+    }
+
+    this.processRefIdObserver = this.bindingEngine
+      .propertyObserver(this.businessObjInPanel.processRef, 'id')
+      .subscribe((newId: string) => {
+        this.processRefId = newId;
+      });
 
     this.validationController.subscribe((event: ValidateEvent) => {
       this.validateId(event);
@@ -54,7 +70,7 @@ export class PoolSection implements ISection {
 
   public detached(): void {
     if (this.validationError) {
-      this.businessObjInPanel.processRef.id = this.previousProcessRefId;
+      this.processRefId = this.previousProcessRefId;
       this.validationController.validate();
     }
   }
@@ -74,11 +90,6 @@ export class PoolSection implements ISection {
 
   public isSuitableForElement(element: IShape): boolean {
     return this.elementIsParticipant(element);
-  }
-
-  public validate(): void {
-    this.validationController.validate();
-    this.publishDiagramChange();
   }
 
   public publishDiagramChange(): void {
@@ -112,6 +123,16 @@ export class PoolSection implements ISection {
     }
   }
 
+  public updateId(): void {
+    this.validationController.validate();
+    if (this.validationController.errors.length > 0) {
+      return;
+    }
+
+    this.businessObjInPanel.processRef.id = this.processRefId;
+    this.publishDiagramChange();
+  }
+
   private formIdIsUnique(id: string): boolean {
     const elementRegistry: IElementRegistry = this.modeler.get('elementRegistry');
     const elementsWithSameId: Array<IShape> = elementRegistry.filter((element: IShape) => {
@@ -134,14 +155,14 @@ export class PoolSection implements ISection {
   }
 
   private setValidationRules(): void {
-    ValidationRules.ensure((businessObject: IModdleElement) => businessObject.id)
+    ValidationRules.ensure((poolSection: PoolSection) => poolSection.processRefId)
       .displayName('processId')
       .required()
       .withMessage('Process-ID cannot be blank.')
       .then()
       .satisfies((id: string) => this.formIdIsUnique(id) && this.isProcessIdUnique(id))
       .withMessage('Process-ID already exists.')
-      .on(this.businessObjInPanel.processRef);
+      .on(this);
   }
 
   private persistModalOptionToLocalStorage(): void {
