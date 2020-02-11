42fd847ffd99c059ce391937062cd9056fb57f20
Alex 😎 <alexander.kasten@5minds.de>
2020-02-11 15:07:11 +0100

    :fire: Remove unnecessary code from think

--- src/modules/think/think.ts
+++ src/modules/think/think.ts
@@ -3,7 +3,6 @@ import {activationStrategy} from 'aurelia-router';
 
 import {ISolutionEntry, ISolutionService, NotificationType} from '../../contracts/index';
 import {NotificationService} from '../../services/notification-service/notification.service';
-import {isRunningInElectron} from '../../services/is-running-in-electron-module/is-running-in-electron.module';
 
 export interface IThinkRouteParameters {
   view?: string;
@@ -20,10 +19,6 @@ export class Think {
   private solutionService: ISolutionService;
   private notificationService: NotificationService;
 
-  private diagramSelected: boolean = false;
-
-  private ipcRenderer: any;
-
   constructor(solutionService: ISolutionService, notificationService: NotificationService) {
     this.solutionService = solutionService;
     this.notificationService = notificationService;
@@ -31,8 +26,6 @@ export class Think {
 
   public async canActivate(routeParameters: IThinkRouteParameters): Promise<boolean> {
     const solutionUriIsSet: boolean = routeParameters.solutionUri !== undefined;
-
-    this.diagramSelected = routeParameters.diagramName !== undefined;
 
     const solutionUri: string = solutionUriIsSet
       ? routeParameters.solutionUri
@@ -57,26 +50,9 @@ export class Think {
 
   public activate(): void {
     this.showDiagramList = true;
-
-    if (isRunningInElectron()) {
-      this.ipcRenderer = (window as any).nodeRequire('electron').ipcRenderer;
-      this.ipcRenderer.on('menubar__start_close_diagram', this.closeBpmnStudio);
-    }
-  }
-
-  public deactivate(): void {
-    if (isRunningInElectron()) {
-      this.ipcRenderer.removeListener('menubar__start_close_diagram', this.closeBpmnStudio);
-    }
   }
 
   public determineActivationStrategy(): string {
     return activationStrategy.replace;
   }
-
-  private closeBpmnStudio: Function = (): void => {
-    if (!this.diagramSelected) {
-      this.ipcRenderer.send('close_bpmn-studio');
-    }
-  };
 }
