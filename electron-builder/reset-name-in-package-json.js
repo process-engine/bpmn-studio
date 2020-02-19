cbb0d54ceb74416727d893e25cec0e952df73593
Alex 😎 <alexander.kasten@5minds.de>
2020-02-19 15:35:17 +0100

    :recycle: Adjust electron-builder script files

--- electron-builder/reset-name-in-package-json.js
+++ electron-builder/reset-name-in-package-json.js
@@ -4,12 +4,18 @@ const {getReleaseChannelSuffix} = require('./release');
 
 const releaseChannelSuffix = getReleaseChannelSuffix();
 
+const MODE = process.env.MODE;
+const portableName = MODE ? `-${MODE}` : '';
+
 fs.readFile('package.json', 'utf8', (err, data) => {
   if (err) {
     throw err;
   }
 
-  const dataWithNewName = data.replace(`  "name": "bpmn-studio${releaseChannelSuffix}",`, '  "name": "bpmn-studio",');
+  const dataWithNewName = data.replace(
+    `  "name": "bpmn-studio${releaseChannelSuffix}${portableName}",`,
+    '  "name": "bpmn-studio",',
+  );
 
   fs.writeFile('package.json', dataWithNewName, (errWrite) => {
     if (errWrite) {
--- electron-builder/set-name-in-package-json.js
+++ electron-builder/set-name-in-package-json.js
@@ -3,13 +3,19 @@ const fs = require('fs');
 const {getReleaseChannelSuffix} = require('./release');
 
 const releaseChannelSuffix = getReleaseChannelSuffix();
+const MODE = process.env.MODE;
+
+const portableName = MODE ? `-${MODE}` : '';
 
 fs.readFile('package.json', 'utf8', (err, data) => {
   if (err) {
     throw err;
   }
 
-  const dataWithNewName = data.replace('  "name": "bpmn-studio",', `  "name": "bpmn-studio${releaseChannelSuffix}",`);
+  const dataWithNewName = data.replace(
+    '  "name": "bpmn-studio",',
+    `  "name": "bpmn-studio${releaseChannelSuffix}${portableName}",`,
+  );
 
   fs.writeFile('package.json', dataWithNewName, (errWrite) => {
     if (errWrite) {
