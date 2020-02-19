56d6d2f83722ab7ce4f1b95d04b2de19d09a9c6c
Steffen Knaup <steffen.knaup@5minds.de>
2020-02-19 13:45:38 +0100

    :lipstick: Improve Readability
    Co-Authored-By: René Föhring <rene.foehring@5minds.de>

--- src/services/help-modal-service/help-texts/script-task-token-usage.ts
+++ src/services/help-modal-service/help-texts/script-task-token-usage.ts
@@ -3,5 +3,10 @@ import {HelpText} from '../../../contracts/index';
 export const ScriptTaskTokenUsageHelpText: HelpText = {
   title: 'How to use the token',
   message:
-    'The data of previous tasks can be accessed via the token.\nIn order to do so make use of:\n"token.current" - In order to use the data of the previous tasks\n"token.history.<id-of-previous-task>" - In order to use the data of a specific tasks.',
+    `The data of previous tasks can be accessed via the token.
+    
+    In order to do so make use of:
+    
+    "token.current" - In order to use the data of the previous tasks
+    "token.history.<id-of-previous-task>" - In order to use the data of a specific tasks.`,
 };
