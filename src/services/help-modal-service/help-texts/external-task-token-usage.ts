3f4e7123c228cd9f84a0becc41f4955d8400eb3f
Alexander Kasten <alexander.kasten@5minds.de>
2020-03-10 10:05:15 +0100

    :recycle: Add description for token usage

--- src/services/help-modal-service/help-texts/external-task-token-usage.ts
+++ src/services/help-modal-service/help-texts/external-task-token-usage.ts
@@ -6,7 +6,12 @@ export const ExternalTaskTokenUsage: HelpText = {
   This progression and state accumulation can be visualized as a token moving along the execution's path in the diagram.
 
   The data of previous tasks can be accessed through the token.
-  
+
+  In order to do so make use of:
+
+  "token.current" - In order to use the data of the previous tasks.
+  "token.history.<id-of-previous-task>" - In order to use the data of a specific tasks.
+
   Examples of using the token as a payload or topic for external tasks:
 
     1. To use the given user data from previous tasks, e.g. an account registration worker:
