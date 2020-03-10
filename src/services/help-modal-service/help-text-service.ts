62eb3a3ee598b280601b8a46f1cf5ada76b19a62
Alexander Kasten <alexander.kasten@5minds.de>
2020-03-10 09:33:42 +0100

    Update src/contracts/help-text/help-text-id.ts
    Co-Authored-By: Steffen Knaup <steffenkn@users.noreply.github.com>

--- src/services/help-modal-service/help-text-service.ts
+++ src/services/help-modal-service/help-text-service.ts
@@ -1,14 +1,14 @@
 import {UserTaskUsage} from './help-texts/usertask-usage';
 import {ExternalTaskTokenUsage} from './help-texts/external-task-token-usage';
-import {ScriptTaskTokenUsageHelpText} from './help-texts/script-task-token-usage';
+import {ScriptTaskTokenUsage} from './help-texts/script-task-token-usage';
 
 import {HelpText, HelpTextId} from '../../contracts/index';
 
 export class HelpTextService {
   public getHelpTextById(helpTextId: HelpTextId): HelpText {
     switch (helpTextId) {
-      case HelpTextId.ScriptTaskTokenUsageHelpText:
-        return ScriptTaskTokenUsageHelpText;
+      case HelpTextId.ScriptTaskTokenUsage:
+        return ScriptTaskTokenUsage;
       case HelpTextId.ExternalTaskTokenUsage:
         return ExternalTaskTokenUsage;
       case HelpTextId.UserTaskUsage:
