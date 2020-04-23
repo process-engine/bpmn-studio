7045912b662d888c98f5713d8d3646a9b23eb741
Alexander Kasten <alexander.kasten@5minds.de>
2020-04-23 12:15:30 +0200

    :sparkles: Add copy button to copy values to clipboard

--- src/modules/inspect/task-list/task-list.ts
+++ src/modules/inspect/task-list/task-list.ts
@@ -274,6 +274,11 @@ export class TaskList {
     this.taskInformation = Object.entries(taskInformationObject);
   }
 
+  public async copyToClipboard(text: string): Promise<void> {
+    console.log(text);
+    await navigator.clipboard.writeText(text);
+  }
+
   private convertToTaskInformationObject(task: TaskListEntry): any {
     return {
       name: task.name,
