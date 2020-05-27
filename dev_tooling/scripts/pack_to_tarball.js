903e419f8a643bdb33beb34d4105787dba3c366b
Alexander Kasten <alexander.kasten@5minds.de>
2020-05-27 12:33:22 +0200

    :recycle: Rename config to configs

--- dev_tooling/scripts/pack_to_tarball.js
+++ dev_tooling/scripts/pack_to_tarball.js
@@ -3,7 +3,7 @@ const os = require('os');
 
 const packageName = 'bpmn-studio.tar.gz';
 
-const tarballCommand = `tar -czf ${packageName} bin build config dist node_modules src/resources index.html favicon.ico package-lock.json package.json README.md`;
+const tarballCommand = `tar -czf ${packageName} bin build configs dist node_modules src/resources index.html favicon.ico package-lock.json package.json README.md`;
 
 exec(tarballCommand)
   .then(() => console.log('Success!'))
