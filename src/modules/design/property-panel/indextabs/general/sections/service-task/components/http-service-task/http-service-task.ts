db456199a507f47c4f1ea313530f682a5c5d9485
Alexander Kasten <alexander.kasten@5minds.de>
2020-09-23 11:12:34 +0200

    :recycle: Adjust reading and writing param properties like in atlas studio

--- src/modules/design/property-panel/indextabs/general/sections/service-task/components/http-service-task/http-service-task.ts
+++ src/modules/design/property-panel/indextabs/general/sections/service-task/components/http-service-task/http-service-task.ts
@@ -14,6 +14,13 @@ interface IAuthParameters {
   };
 }
 
+type HttpServiceTaskParams = {
+  url: string;
+  body: string;
+  contentType: string;
+  authorization: string;
+};
+
 @inject(EventAggregator)
 export class HttpServiceTask {
   @bindable() public model: IPageModel;
@@ -54,7 +61,11 @@ export class HttpServiceTask {
       this.selectedHttpContentType = undefined;
     }
 
-    this.serviceTaskService.getProperty('params').value = this.getParamsFromInput();
+    this.serviceTaskService.updateOrCreateProperty('params', this.getParamsFromInput());
+    this.serviceTaskService.updateOrCreateProperty('studio.paramUrl', this.selectedHttpUrl);
+    this.serviceTaskService.updateOrCreateProperty('studio.paramBody', this.selectedHttpBody);
+    this.serviceTaskService.updateOrCreateProperty('studio.paramContentType', this.selectedHttpContentType);
+    this.serviceTaskService.updateOrCreateProperty('studio.paramAuthorization', this.selectedHttpAuth);
     this.publishDiagramChange();
   }
 
@@ -68,7 +79,6 @@ export class HttpServiceTask {
 
   private initHttpServiceTask(): void {
     const methodPropertyExists: boolean = this.serviceTaskService.getProperty('method') !== undefined;
-    const paramPropertyExists: boolean = this.serviceTaskService.getProperty('params') !== undefined;
 
     if (methodPropertyExists) {
       this.selectedHttpMethod = this.serviceTaskService.getProperty('method').value;
@@ -76,10 +86,17 @@ export class HttpServiceTask {
       this.serviceTaskService.createProperty('method');
     }
 
-    if (paramPropertyExists) {
-      this.fillVariablesFromParam(this.serviceTaskService.getProperty('params').value);
-    } else {
-      this.serviceTaskService.createProperty('params');
+    let params = this.getParamProperties();
+    if (params == null) {
+      params = this.unreliableGetPropertiesFromParams();
+    }
+
+    const paramsExist = params != null;
+    if (paramsExist) {
+      this.selectedHttpUrl = params.url;
+      this.selectedHttpBody = params.body;
+      this.selectedHttpContentType = params.contentType;
+      this.selectedHttpAuth = params.authorization;
     }
   }
 
@@ -121,60 +138,90 @@ export class HttpServiceTask {
     return returnValue;
   }
 
-  private fillVariablesFromParam(params: string): void {
-    let parsedParams = {};
-    try {
-      parsedParams = JSON.parse(params);
-      this.selectedHttpUrl = parsedParams[0];
-      this.selectedHttpBody =
-        typeof parsedParams[1] === 'object' ? JSON.stringify(parsedParams[1], null, 2) : parsedParams[1];
-      this.selectedHttpContentType = parsedParams[2]?.headers['Content-Type'];
-      this.selectedHttpAuth = parsedParams[2]?.headers.Authorization;
-    } catch {
-      let stringParams = params.trim();
+  private getParamProperties(): HttpServiceTaskParams | null {
+    const paramUrl = this.serviceTaskService.getProperty('studio.paramUrl')?.value;
+    const paramBody = this.serviceTaskService.getProperty('studio.paramBody')?.value;
+    const paramContentType = this.serviceTaskService.getProperty('studio.paramContentType')?.value;
+    const paramAuthorization = this.serviceTaskService.getProperty('studio.paramAuthorization')?.value;
 
-      const hasHeaders = stringParams.includes('{"headers');
-      let headers = '';
-      if (hasHeaders) {
-        const indexOfHeaders = stringParams.indexOf('{"headers"');
-        headers = stringParams.substring(indexOfHeaders).replace(/]$/g, '');
-        try {
-          const parsedHeaders = JSON.parse(headers);
-          this.selectedHttpContentType = parsedHeaders.headers['Content-Type'];
-          this.selectedHttpAuth = parsedHeaders.headers.Authorization;
-        } catch {
-          // do nothing
-        }
-        stringParams = stringParams.substring(0, indexOfHeaders);
-      }
-
-      const hasBody = stringParams.includes(',');
-      let newBody = '';
-      if (hasBody) {
-        const indexOfComma = stringParams.indexOf(',');
-        newBody = stringParams
-          .substring(indexOfComma)
-          .trim()
-          .replace(/,$/g, '');
-        newBody = newBody.replace(/^,/, '').trim();
-
-        const isPossibleObject = newBody.startsWith('{') && newBody.endsWith('}');
-        if (!isPossibleObject) {
-          newBody = newBody.replace(/]$/g, '').replace(/^"|"$/g, '');
-        }
-
-        this.selectedHttpBody = newBody;
-        stringParams = stringParams.substring(0, indexOfComma);
-      }
-
-      const noCommaInStringParams = !stringParams.includes(',');
-      if (noCommaInStringParams) {
-        this.selectedHttpUrl = stringParams
-          .replace('[', '')
-          .replace(/]$/g, '')
-          .replace(/"/g, '')
-          .trim();
-      }
+    if (paramUrl == null && paramBody == null && paramContentType == null && paramAuthorization == null) {
+      return null;
     }
+
+    return {
+      url: paramUrl,
+      body: paramBody,
+      contentType: paramContentType,
+      authorization: paramAuthorization,
+    };
+  }
+
+  private unreliableGetPropertiesFromParams(): HttpServiceTaskParams | null {
+    const params = this.serviceTaskService.getProperty('params')?.value;
+
+    if (params == null) {
+      return null;
+    }
+
+    let url;
+    let body;
+    let contentType;
+    let authorization;
+
+    let stringParams: string = params?.trim() || '';
+
+    const hasHeaders = stringParams.includes('{"headers');
+    let headers = '';
+    if (hasHeaders) {
+      const indexOfHeaders = stringParams.indexOf('{"headers"');
+      headers = stringParams.substring(indexOfHeaders).replace(/]$/g, '');
+      try {
+        const parsedHeaders = JSON.parse(headers);
+        contentType = parsedHeaders.headers['Content-Type'];
+        authorization = parsedHeaders.headers.Authorization;
+      } catch {
+        // do nothing
+      }
+      stringParams = stringParams.substring(0, indexOfHeaders);
+    }
+
+    const hasBody = stringParams.includes(',');
+    let newBody = '';
+    if (hasBody) {
+      const indexOfComma = stringParams.indexOf(',');
+      newBody = stringParams
+        .substring(indexOfComma)
+        .trim()
+        .replace(/,$/g, '');
+      newBody = newBody.replace(/^,/, '').trim();
+
+      const isPossibleObject = newBody.startsWith('{') && newBody.endsWith('}');
+      if (!isPossibleObject) {
+        newBody = newBody.replace(/]$/g, '').replace(/^"|"$/g, '');
+      }
+
+      body = newBody;
+      stringParams = stringParams.substring(0, indexOfComma);
+    }
+
+    const noCommaInStringParams = !stringParams.includes(',');
+    if (noCommaInStringParams) {
+      url = stringParams
+        .replace('[', '')
+        .replace(/]$/g, '')
+        .replace(/"/g, '')
+        .trim();
+    }
+
+    if (url == null && body == null && contentType == null && authorization == null) {
+      return null;
+    }
+
+    return {
+      url: url,
+      body: body,
+      contentType: contentType,
+      authorization: authorization,
+    };
   }
 }
